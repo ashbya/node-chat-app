7c54ee7d1e5491ac043cf6282667138349f9c9f2
ashbya <avery.ashby@gmail.com>
2018-07-24 22:12:13 -0500

    Add geolocation support and test

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
     it('should generate the correct message object', () => {
@@ -9,6 +9,21 @@ describe('generateMessage', () => {
       expect(msg.from).toBe('test1');
       expect(msg.text).toBe('Test message.');
       console.log(msg);
+      //expect(msg).toInclude({from: 'test1', text: 'Test message.'});
+      expect(typeof msg.createdAt).toBe('number');
+    });
+});
+
+describe('generateLocationMessage', () => {
+    it('should generate the correct location object', () => {
+      var from = 'TestAdmin';
+      var latitude = 35;
+      var longitude =  86;
+      var url = `https://www.google.com/maps?q=${latitude},${longitude}`;
+      var msg = generateLocationMessage(from, latitude, longitude);
+
+      expect(msg.from).toBe(from);
+      expect(msg.url).toBe(url);
       //expect(msg).toInclude({from: 'test1', text: 'Test message.'});
       expect(typeof msg.createdAt).toBe('number');
     });
